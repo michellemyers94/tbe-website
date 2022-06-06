8169db6d45e16df53f93841b6136722867cf4b65
Michelle Myers <michelleliumyers@gmail.com>
2022-06-05 22:09:18 -0700

    email contact form

---
+++ src/routes/contact-success.svelte
@@ -0,0 +1,10 @@
+<body>
+
+<h4>Contact</h4>
+
+<div class="success-message">
+<h2>Thank you for your message! You will receive a response as soon as possible!</h2>
+</div>
+
+
+</body>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,4 +1,19 @@
 <body>
 
 <h4>Contact</h4>
+
+
+
+<form action="https://api.staticforms.xyz/submit" method="post">
+    <input type="text" name="name" placeholder="Your Name">
+    <input type="text" name="email" placeholder="Your Email" />
+    <input type="text" name="subject" placeholder="Your Subject" />
+    <textarea name="message" class="submit-form" placeholder="Enter Your Message"></textarea>
+    <input type="text" name="honeypot" style="display:none">
+    <input type="hidden" name="accessKey" value="2f903c59-3647-4a7a-8244-5a73c5d27308">
+
+    <input type="hidden" name="replyTo" value="theblankexperiment@outlook.com">
+    <input type="hidden" name="redirectTo" value="https://tbe-website.vercel.app/contact-success">
+    <input type="submit" value="Submit" />
+</form>
 </body>
