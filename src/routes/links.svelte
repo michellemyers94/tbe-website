046db1bf81bff4b845fbd4f6fdceaaa036f8c360
Michelle Myers <michelleliumyers@gmail.com>
2022-09-05 15:02:47 -0700

    links video

--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -1,22 +1,27 @@
 <script>
     import "../styles/_links.css";
+    import logo from '../../static/white-logo.png';
     import { reveal } from 'svelte-reveal';
     import FaTwitter from 'svelte-icons/fa/FaTwitter.svelte';
     import FaYoutube from 'svelte-icons/fa/FaYoutube.svelte';
     import FaFacebook from 'svelte-icons/fa/FaFacebook.svelte';
     import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte'
+    import VideoBackground from '../../static/links-video-background-lite.mp4';
 
 
 
 </script>
 
-
+<video autoplay muted loop id="myVideo">
+    <source src={VideoBackground} type="video/mp4">
+</video>
 
 
 <body>
-<h4>The _____ Experiment</h4>
+<a href="/index"><img src={logo} alt="tbe logo" class="link-logo" /></a>
+<p>New Music Ensemble</p>
 <div class="links">
-<a href="https://tbe-website.vercel.app" target="_blank"><button use:reveal={{ delay: 50}}>Check out our new website</button></a>
+<a href="/index" target="_blank"><button use:reveal={{ delay: 50}}>Check out our new website</button></a>
     <a href="https://mailchi.mp/928f57c3d1f2/style-guide" target="_blank"><button use:reveal={{ delay: 250}}>Download our style guide</button></a>
     <a href="https://mailchi.mp/d0ece2b7da16/theblankexperiment" target="_blank"><button use:reveal={{ delay: 450}}>Subscribe to our email list</button></a>
     <a href="https://www.youtube.com/channel/UCyRGtoDqCPV-uHDXXxkPJ9A" target="_blank"><button use:reveal={{ delay: 550}}>Check out our Youtube Channel</button></a>
@@ -38,3 +43,8 @@
 
 </body>
 
+<style>
+    body {
+        background: none;
+    }
+</style>
