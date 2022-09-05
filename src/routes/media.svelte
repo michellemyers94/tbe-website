4ff135137352800a37666d05743a8034c1e9d4ca
Michelle Myers <michelleliumyers@gmail.com>
2022-09-05 01:50:15 -0700

    repertoire table

--- src/routes/media.svelte
+++ src/routes/media.svelte
@@ -6,9 +6,336 @@
 
  <h4 use:reveal={{ delay: 50 }}>media &amp; repertoire</h4>
 
+<p class="podcast">Debut album <span class="tbe-podcast">"Conversations"</span> available on all major streaming platforms</p>
+<div class="repertoire-table">
+<p>Our Repertoire Portfolio</p>
+<table class="repertoire">
+ <tr>
+  <th>First Name</th>
+  <th>Last Name</th>
+  <th>Piece Title</th>
+  <th>Premiere?</th>
+  <th>Video</th>
+  <th>Composer Website</th>
 
+ </tr>
+ 
+ <tr>
+  <td>David</td>
+  <td>Bohn</td>
+  <td>Island</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/8f9oVl3vBCE?start=283" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td>N/A</td>
+ </tr>
 
+ <tr>
+  <td>Benjamin</td>
+  <td>Damann</td>
+  <td>pounceupcrack</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/26HJ0-MXPOY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.bendamann.com/" target="_blank">Composer Website</a></td>
+ </tr>
 
+ <tr>
+  <td>Connor</td>
+  <td>Gibbs</td>
+  <td>Nostalgia in a Field of Flowers</td>
+  <td>Yes</td>
+  <td><a href="https://youtu.be/5RV3vcAhKlI?t=7323" target="_blank">Click Here to Watch Video</a></td>
+  <td><a href="https://www.connorgibbs.com/">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Jacobo</td>
+  <td>Salvador Alan</td>
+  <td>Hushed Agony</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/ibvOzxFGubY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.musicjacobo.com/" target="_blank">Composer Website</a> </td>
+ </tr>
+
+ <tr>
+  <td>Andy</td>
+  <td>Juttonen</td>
+  <td>Stressed and Stressed</td>
+  <td>Yes</td>
+  <td><a href="https://youtu.be/5RV3vcAhKlI?t=7698" target="_blank">Click Here to Watch Video</a></td>
+  <td>N/A</td>
+ </tr>
+
+ <tr>
+  <td>Robert</td>
+  <td>Lemay</td>
+  <td>Faux Fuyant</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/YJr_RZJOaC8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://en.wikipedia.org/wiki/Robert_Lemay" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Robert</td>
+  <td>Lemay</td>
+  <td>Hors-jeu</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/8BIeGjnx58M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://en.wikipedia.org/wiki/Robert_Lemay" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Jimmy</td>
+  <td>Levi</td>
+  <td>The Alibi</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/p-XpUueCKqg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://jimmylevicomposer.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Mason</td>
+  <td>Quinn</td>
+  <td>Syntax Error</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/Qjiki7Ckf-o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.masonianmusic.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Isaac</td>
+  <td>Mayhew</td>
+  <td>...about nothing...</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/AbGEZ0ze-IY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.isaacmayhewcomposition.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Tyler</td>
+  <td>Mazone</td>
+  <td>I Want Answers!</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/gXHvHqJiJc8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.tylermazone.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Zachary</td>
+  <td>McDonald</td>
+  <td>They died for their beliefs that killed them</td>
+  <td>Yes</td>
+  <td><a href="https://youtu.be/5RV3vcAhKlI?t=2426" target="_blank">Click Here To Watch Video</a> </td>
+  <td><a href="https://www.zacharybmcdonald.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Emily</td>
+  <td>McPherson</td>
+  <td>6:31 (Daybreak)</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/JJfuzdkfH5g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://emilymcpherson-composer.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Everette</td>
+  <td>Minchew</td>
+  <td>Reems Creek Idyll</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/IxLwNtgdK-8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.everetteminchew.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Brandon</td>
+  <td>Nelson</td>
+  <td>Solid, Liquid, Gas, Plasma</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/f7WcnzVA84U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://bnelsonmusic.wordpress.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Kincaid</td>
+  <td>Rabb</td>
+  <td>Say My Name</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/oaqE-ZlKvhQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.kincaidrabb.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Selena</td>
+  <td>Ryan</td>
+  <td>Kadupul</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/oMUYZFDFKKw?start=67" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td>N/A</td>
+ </tr>
+
+ <tr>
+  <td>Jamie Leigh</td>
+  <td>Sampson</td>
+  <td>(9:01 Rush) Elevator Music</td>
+  <td></td>
+  <td><a href="https://youtu.be/Vn-MYoJVj6k?t=1606" target="_blank">Click Here to Watch Video</a></td>
+  <td><a href="http://jamieleighsampson.weebly.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Jamie Leigh</td>
+  <td>Sampson</td>
+  <td>Necessity is the Mother of All</td>
+  <td></td>
+  <td><a href="https://youtu.be/8f9oVl3vBCE?t=51" target="_blank">Click Here to Watch Video</a></td>
+  <td><a href="http://jamieleighsampson.weebly.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Connor</td>
+  <td>Scroggins</td>
+  <td>Routine Impact</td>
+  <td>Yes</td>
+  <td><a href="https://youtu.be/5RV3vcAhKlI?t=2789" target="_blank">Click Here to Watch Video</a></td>
+  <td>N/A</td>
+ </tr>
+
+ <tr>
+  <td>Pranav</td>
+  <td>Sivakumar</td>
+  <td>Silent Shores</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/o0dPM8oGwM4?start=376" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://composingsultasto.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Andrew Martin</td>
+  <td>Smith</td>
+  <td>November 5, 2012</td>
+  <td></td>
+  <td><a href="https://youtu.be/5RV3vcAhKlI?t=6011" target="_blank">Click Here to Watch Video</a></td>
+  <td><a href="https://www.andrewmartinsmith.com/" target="_blank">Composer Website</a></td>
+ </tr>
+ 
+ <tr>
+  <td>Hayden</td>
+  <td>Stacki</td>
+  <td>Reeling</td>
+  <td>Yes</td>
+  <td><a href="https://youtu.be/5RV3vcAhKlI?t=5507" target="_blank">Click Here to Watch Video</a></td>
+  <td><a href="https://www.haydenstacki.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Emily Joy</td>
+  <td>Sullivan</td>
+  <td>Elegy</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/i0lB5bWiOP4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.adjectivenewmusic.com/sullivan.html" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Brendan</td>
+  <td>Sweeney</td>
+  <td>Safe Passage</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/5rBwOwqlgMo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.brendansweeneymusic.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Shahrzad</td>
+  <td>Talebi</td>
+  <td>Lullaby for a dried land</td>
+  <td>Yes</td>
+  <td><a href="https://youtu.be/5RV3vcAhKlI?t=6368" target="_blank">Click Here to Watch Video</a></td>
+  <td>N/A</td>
+ </tr>
+
+ <tr>
+  <td>Josh</td>
+  <td>Trentadue</td>
+  <td>Antikythera</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/WmMChgPflqI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.joshtrentadue.com" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Josh</td>
+  <td>Trentadue</td>
+  <td>Conversations</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/cnCpml8Gu8U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.joshtrentadue.com" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Josh</td>
+  <td>Trentadue</td>
+  <td>Inferno 7.0</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/TjwFoTLBkks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.joshtrentadue.com" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Josh</td>
+  <td>Trentadue</td>
+  <td>Meditation and Hymn to Nature</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/oMUYZFDFKKw?start=705" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.joshtrentadue.com" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Josh</td>
+  <td>Trentadue</td>
+  <td>Song</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/XJEx68CyuEM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.joshtrentadue.com" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Trey</td>
+  <td>Tillotson</td>
+  <td>Echo</td>
+  <td>Yes</td>
+  <td><iframe src="https://www.youtube.com/embed/Ky2XOws5fsE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://treytillotson.square.site/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Cassie</td>
+  <td>Wieland</td>
+  <td>Go Together</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/IsIn3kbXxCc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://cassiewieland.com/" target="_blank">Composer Website</a></td>
+ </tr>
+
+ <tr>
+  <td>Ian</td>
+  <td>Wiese</td>
+  <td>Twin Colors</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/n8l1T8byr6g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td><a href="https://www.ianwiese.com/" target="_blank">Composer Website</a></td>
+ </tr>
+ 
+ <tr>
+  <td>Jason</td>
+  <td>Zhang</td>
+  <td>Oh My Gourd</td>
+  <td></td>
+  <td><iframe src="https://www.youtube.com/embed/o0dPM8oGwM4?start=74" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  <td>N/A</td>
+ </tr>
+</table>
+</div>
 
 <style>
  body {
