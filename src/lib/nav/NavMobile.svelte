3de2a0b586395123ad59923a8878c839ce51eeca
Michelle Myers <michelleliumyers@gmail.com>
2022-09-04 23:08:17 -0700

    toggle for menu items

--- src/lib/nav/NavMobile.svelte
+++ src/lib/nav/NavMobile.svelte
@@ -2,6 +2,7 @@
 
 
 let active =false;
+
 </script>
 
 
@@ -26,12 +27,12 @@ let active =false;
 
     <aside class:active>
         <ul>
-            <li><a href=".">home</a></li>
-            <li><a href="/about">about</a></li>
-            <li><a href="/media">media</a></li>
-            <li><a href="https://facebook.us5.list-manage.com/subscribe/post?u=1b6151b7dbe8fc8f8f7d0df7a&id=cf4dfa7be9&f_id=00aadae6f0" target="_blank">subscribe</a></li>
-            <li><a href="/contact">contact us</a></li>
-            <li><a href="/moon-dances" class="special">Moon Dances</a></li>
+            <li><a href="." on:click={()=> active = !active}>home</a></li>
+            <li><a href="/about" on:click={()=> active = !active}>about</a></li>
+            <li><a href="/media" on:click={()=> active = !active}>media</a></li>
+            <li><a href="https://facebook.us5.list-manage.com/subscribe/post?u=1b6151b7dbe8fc8f8f7d0df7a&id=cf4dfa7be9&f_id=00aadae6f0" target="_blank" on:click={()=> active = !active}>subscribe</a></li>
+            <li><a href="/contact" on:click={()=> active = !active}>contact us</a></li>
+            <li><a href="/moon-dances" on:click={()=> active = !active}>Moon Dances</a></li>
         </ul>
     </aside>
 </div>
