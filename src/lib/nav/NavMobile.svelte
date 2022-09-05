56f400187d5c69a1a661c76aead3831bd5d13471
Michelle Myers <michelleliumyers@gmail.com>
2022-09-04 22:59:38 -0700

    mobile nav bar complete?

--- src/lib/nav/NavMobile.svelte
+++ src/lib/nav/NavMobile.svelte
@@ -26,12 +26,12 @@ let active =false;
 
     <aside class:active>
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Media</li>
-            <li>Subscribe</li>
-            <li>Contact Us</li>
-            <li>Moon Dances</li>
+            <li><a href=".">home</a></li>
+            <li><a href="/about">about</a></li>
+            <li><a href="/media">media</a></li>
+            <li><a href="https://facebook.us5.list-manage.com/subscribe/post?u=1b6151b7dbe8fc8f8f7d0df7a&id=cf4dfa7be9&f_id=00aadae6f0" target="_blank">subscribe</a></li>
+            <li><a href="/contact">contact us</a></li>
+            <li><a href="/moon-dances" class="special">Moon Dances</a></li>
         </ul>
     </aside>
 </div>
