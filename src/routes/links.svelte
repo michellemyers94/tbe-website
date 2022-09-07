cd8e97447ea36352bf67b063fe70117a23c36958
Michelle Myers <michelleliumyers@gmail.com>
2022-09-07 11:22:47 -0700

    links url updated

--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -21,11 +21,11 @@
 <a href="/index"><img src={logo} alt="tbe logo" class="link-logo" /></a>
 <p>New Music Ensemble</p>
 <div class="links">
-<a href="/index" target="_blank"><button use:reveal={{ delay: 50}}>Check out our new website</button></a>
+<a href="/index" target="_blank"><button use:reveal={{ delay: 50}} class="active">Check out our new website</button></a>
     <a href="https://mailchi.mp/928f57c3d1f2/style-guide" target="_blank"><button use:reveal={{ delay: 250}}>Download our style guide</button></a>
     <a href="https://mailchi.mp/d0ece2b7da16/theblankexperiment" target="_blank"><button use:reveal={{ delay: 450}}>Subscribe to our email list</button></a>
     <a href="https://www.youtube.com/channel/UCyRGtoDqCPV-uHDXXxkPJ9A" target="_blank"><button use:reveal={{ delay: 550}}>Check out our Youtube Channel</button></a>
-    <a href="https://www.youtube.com/watch?v=IsIn3kbXxCc" target="_blank"><button use:reveal={{ delay: 650}} class="active">New Youtube video: Cassie Wieland - Go Together</button></a>
+    <a href="https://youtu.be/BehU_9EMq4E" target="_blank"><button use:reveal={{ delay: 650}}>New Youtube video: Cole Ryes - Frayed Wings</button></a>
 
 
     <div class="urls">
