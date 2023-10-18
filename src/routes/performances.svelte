7602375df319b030630066ecfe04d2ee79bc1d55
Michelle Myers <michelleliumyers@gmail.com>
2023-10-18 16:16:40 -0700

    update poster

--- src/routes/performances.svelte
+++ src/routes/performances.svelte
@@ -1,15 +1,13 @@
 <script>
     // import "../styles/_background.css";
     import "../styles/_about-background.css";
-    import moon_dance from '../../static/moon-dances-announcement.png';
-    import kaiser_poster from '../../static/kaiser_poster.png';
-    import classical_revolution from '../../static/classical_revolution.png';
+    import present_poster from '../../static/the-present.png';
     import { reveal } from 'svelte-reveal';
 </script>
 
 <body>
 
-<h4 use:reveal={{ delay: 50 }} class="composer-bootcamp">Kaiser Gallery's June Concert Series <em>Melodic Mixtures</em></h4>
+<h4 use:reveal={{ delay: 50 }} class="composer-bootcamp">Virtual Recital Series: <em>the present...</em></h4>
 <p>June 27, 2023</p>
 <p>7pm EDT</p>
 <p>Location: 2418 Professor Ave, Cleveland OH 44113</p>
@@ -17,19 +15,9 @@
 <p class="member-bio"><a href="https://www.eventbrite.com/e/the-blank-experiment-live-at-kaiser-gallery-tickets-658465948377" class="performance">(Get Tickets)</a></p>
 <a href="https://goo.gl/maps/AEZCsEKZZL7hang9A" target="_blank"><button class="press-kit">Open in Google Maps</button></a>
 <div class="poster">
-    <img src={kaiser_poster} class="poster-image" />
+    <img src={present_poster} class="poster-image" />
 </div>
 
-
-<h4 use:reveal={{ delay: 50 }} class="composer-bootcamp">Classical Revolution Cleveland</h4>
-<p>June 21, 2023</p>
-<p>8pm EDT</p>
-<p>Location: Happy Dog</p>
-<p>Tickets: FREE</p>
-<a href="https://goo.gl/maps/UsPL11dsVXPJkNnN9" target="_blank"><button class="press-kit">Open in Google Maps</button></a>
-<div class="poster">
-    <img src={classical_revolution} class="poster-image" />
-</div>
 
 
 
